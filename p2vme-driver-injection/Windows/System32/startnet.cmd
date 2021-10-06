3031532736c6359ff0a48fcf9a5a3be61c341bd5
Fernando Della Torre <fernando.dellatorre@bs4it.com.br>
2021-10-06 08:00:58 -0300

    Network static IP OK

--- p2vme-driver-injection/Windows/System32/startnet.cmd
+++ p2vme-driver-injection/Windows/System32/startnet.cmd
@@ -1,10 +1,10 @@
 @echo off
-echo "Initializing system..."
+echo Initializing system...
 wpeinit
-echo "Initializing interfaces..."
+echo Initializing interfaces...
 wpeutil InitializeNetwork /NoWait >nul
 netsh interface ipv4 set address "Ethernet" static 12.34.56.78 255.255.255.252 >nul
-echo "Loading..."
+echo Loading...
 ping 127.0.0.1 -n 12 >nul
-echo "Ready"
+echo Ready
 p2vme.cmd
